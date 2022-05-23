0da5f95266f81afd303d8644d8814d254b244f60
Daniel Orshansky <danielorshansky@gmail.com>
2022-05-22 23:56:13 -0500

    uhh

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,45 @@
+use rand::Rng;
 use sdl2::pixels::Color;
 use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
 
 struct Piece {
-    points: Vec<[i8; 2]>,
-    center: usize, // index of the center point
-    size: [u8; 2]
+    points: Vec<[i32; 2]>,
+    origin: [u8; 4] // left, up, right, down
 }
 
 fn create_piece(piece_id: u8) -> Option<Piece> {
     match piece_id {
-        0 => Some(Piece { points: vec![[0, 0], [1, 0], [2, 0], [3, 0]], center: 1, size: [4, 1] }),
-        1 => Some(Piece { points: vec![[0, 0], [1, 0], [2, 0], [1, 1]], center: 3, size: [3, 2] }),
-        2 => Some(Piece { points: vec![[0, 0], [1, 0], [2, 0], [0, 1]], center: 1, size: [3, 2] }),
-        3 => Some(Piece { points: vec![[1, 0], [2, 0], [0, 1], [1, 1]], center: 3, size: [3, 2] }),
-        4 => Some(Piece { points: vec![[0, 0], [1, 0], [0, 1], [1, 1]], center: 2, size: [2, 2] }),
+        0 => Some(Piece { points: vec![[-1, 0], [0, 0], [1, 0], [2, 0]], origin: [1, 0, 3, 1] }),
+        1 => Some(Piece { points: vec![[-1, -1], [0, -1], [1, -1], [0, 0]], origin: [1, 1, 2, 1] }),
+        2 => Some(Piece { points: vec![[-1, 0], [0, 0], [1, 0], [-1, 1]], origin: [1, 0, 2, 2] }),
+        3 => Some(Piece { points: vec![[0, -1], [1, -1], [-1, 0], [0, 0]], origin: [1, 1, 1, 2] }),
+        4 => Some(Piece { points: vec![[0, -1], [1, -1], [0, 0], [1, 0]], origin: [0, 1, 2, 1] }),
         _ => None
     }
 }
 
-fn rotate_piece(piece: &mut Piece) { // rotate clockwise, the rotation formula comes from rotation matrices
-    let cx = piece.points[piece.center][0]; // center x pos
-    let cy = piece.points[piece.center][1]; // center y pos
+fn rotate_cw(piece: &mut Piece) { // rotate clockwise, the rotation formula comes from rotation matrices, note: signs from matrices are flipped b/c positive is down
     for point in piece.points.iter_mut() {
-        *point = [point[1] - cy + cx, cx - point[0] + cy];
+        *point = [-point[1], point[0]];
     }
-    piece.size = [piece.size[1], piece.size[0]];
+    piece.origin.rotate_right(1);
+}
+
+fn rotate_ccw(piece: &mut Piece) { // rotate counterclockwise, the rotation formula comes from rotation matrices, note: signs from matrices are flipped b/c positive is down
+    for point in piece.points.iter_mut() {
+        *point = [point[1], -point[0]];
+    }
+    piece.origin.rotate_left(1);
 }
 
 fn main() {
+    let mut rng = rand::thread_rng();
+    let mut piece = create_piece(rng.gen_range(0..5)).unwrap();
+
     let context = sdl2::init().unwrap();
     let mut canvas = context.video().unwrap().window("Tetris", 480, 840).position_centered().opengl().build().unwrap().into_canvas().build().unwrap();
 
-    canvas.set_draw_color(Color::RGB(0, 0, 255));
     canvas.clear();
     canvas.present();
     
@@ -40,11 +48,20 @@ fn main() {
         for event in events.poll_iter() {
             match event {
                 Event::Quit { .. } => break 'running,
+                Event::KeyDown { keycode: Some(Keycode::S), .. } => rotate_cw(&mut piece),
+                Event::KeyDown { keycode: Some(Keycode::D), .. } => rotate_ccw(&mut piece),
                 _ => {}
             }
         }
 
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
         canvas.clear();
+
+        canvas.set_draw_color(Color::RGB(0, 0, 255));
+        for point in piece.points.iter() {
+            canvas.fill_rect(sdl2::rect::Rect::new(200 - point[0] * 40, 200 - point[1] * 40, 40, 40)).unwrap();
+        }
+
         canvas.present();
     }
 }
